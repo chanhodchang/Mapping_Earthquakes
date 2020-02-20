8de0adaa5d51bef42e26f8cc39cbc9f875899205
Daniel Chang <danielchang@daniels-mbp.lan>
2020-02-19 22:46:11 -0800

    modified logic.js to have markers.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,12 +4,24 @@ console.log('working');
 // Create the map object with a center and zoom level. 
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+// Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: 'yellow',
+    fillOpacity: 0.05,
+    radius: 300
+}).addTo(map);
+
+// Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	id: 'mapbox/streets-v11',
+	id: 'mapbox/dark-v10',
 	accessToken: API_KEY
 });
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
